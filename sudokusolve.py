2e8b8583477c92d5e07e7b789465b686a540720f
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2024-01-29 20:06:12 +0100

    Add tests

---
+++ sudokusolve.py
@@ -0,0 +1,70 @@
+import time
+import functools
+
+# @functools.cache
+@functools.lru_cache
+def sudoku_solve(board):
+    #Find first empty spot and return if no empty spaces exist
+    board = [list(row) for row in board]
+    try:
+        # convert 2d list to 1d, get index of 0 and convert index back to 2d
+        x, y = divmod(sum(board, []).index(0), 9)
+    except ValueError:
+        board = tuple(tuple(row) for row in board)
+        return board
+
+    # Find possible values for empty spot
+    row_nums = set(board[x])
+    col_nums = {row[y] for row in board}
+
+    column_begin = y - y % 3
+    row_begin = x - x % 3
+    column_end = row_begin + 3
+    row_end = column_begin + 3
+
+    block_nums = set(sum((row[column_begin:column_end] for row in board[row_begin:row_end]), []))
+
+
+    options = set(range(1, 10)) - (row_nums | col_nums | block_nums)
+
+    # Solve further on board with value applied
+    for option in options:
+        try:
+            board[x][y] = option
+            board = tuple(tuple(row) for row in board)
+            return sudoku_solve(board)
+        except AttributeError:
+            board = [list(row) for row in board]
+
+            board[x][y] = 0
+    raise AttributeError
+
+if __name__ == '__main__':
+    board0 = [[1, 0, 0, 0, 0, 0, 0, 6, 0],
+            [0, 0, 0, 0, 7, 5, 0, 3, 0],
+            [0, 4, 8, 0, 9, 0, 1, 0, 0],
+            [0, 0, 0, 3, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 1, 0, 0, 0, 9],
+            [0, 0, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 1, 0, 2, 0, 5, 7, 0],
+            [0, 8, 0, 7, 3, 0, 0, 0, 0],
+            [0, 9, 0, 0, 0, 0, 0, 0, 4]]
+
+    board1 = ((7, 8, 0, 4, 0, 0, 1, 2, 0),
+            (6, 0, 0, 0, 7, 5, 0, 0, 9),
+            (0, 0, 0, 6, 0, 1, 0, 7, 8),
+            (0, 0, 7, 0, 4, 0, 2, 6, 0),
+            (0, 0, 1, 0, 5, 0, 9, 3, 0),
+            (9, 0, 4, 0, 6, 0, 0, 0, 5),
+            (0, 7, 0, 3, 0, 0, 0, 1, 2),
+            (1, 2, 0, 0, 0, 7, 4, 0, 0),
+            (0, 4, 9, 2, 0, 6, 0, 0, 7))
+
+    timestamp = time.time()
+    for _ in range(1000000):
+        sudoku_solve(board1)
+        # for r in sudoku_solve(board1):
+        #     print(r)
+        # print("\n")
+
+    print(time.time() - timestamp)
